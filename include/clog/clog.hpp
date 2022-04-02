d94decdc32170cc568fe9489df946ff8a8b758ed
D.Miwa <suconbu@gmail.com>
2022-04-02 16:47:57 +0900

    Remove unused

--- include/clog/clog.hpp
+++ include/clog/clog.hpp
@@ -174,49 +174,6 @@ namespace internal {
         constexpr uint32_t operator"" _murmur3(char const* str, size_t len) { return murmur3(str, len); }
     }
 
-    namespace regex_static {
-        // clang-format off
-
-        // http://cpptruths.blogspot.com/2011/07/compile-time-regex-matcher-using.html
-
-        constexpr int match_c(const char *regexp, const char *text);
-        constexpr int matchhere_c(const char *regexp, const char *text);
-        constexpr int matchstar_c(int c, const char *regexp, const char *text);
-        constexpr int matchend_c(const char * regexp, const char * text);
-
-        constexpr int match_c(const char *regexp, const char *text)
-        {
-            return (regexp[0] == '^') ? matchhere_c(regexp+1, text) : 
-                                        matchend_c(regexp, text);
-        }
-
-        /* matchhere: search for regexp at beginning of text */
-        constexpr int matchhere_c(const char *regexp, const char *text)
-        {
-            return (regexp[0] == '\0') ? 1 : 
-                    (regexp[1] == '*') ? matchstar_c(regexp[0], regexp+2, text) :
-                    (regexp[0] == '$' && regexp[1] == '\0') ? (*text == '\0') :
-                    (*text!='\0' && (regexp[0]=='.' || regexp[0]==*text)) ? 
-                    matchhere_c(regexp+1, text+1) : 0;
-        }
-
-        constexpr int matchend_c(const char * regexp, const char * text)
-        {
-            return matchhere_c(regexp, text) ? 1 : 
-                    (*text == '\0') ? 0 : matchend_c(regexp, text+1);
-        }
-
-        /* matchstar: search for c*regexp at beginning of text */
-        constexpr int matchstar_c(int c, const char * regexp, const char *text)
-        {
-            return matchhere_c(regexp, text) ? 1 : 
-                    (*text != '\0' && (*text == c || c == '.')) ? 
-                    matchstar_c(c, regexp, text+1) : 0;
-        }
-
-        // clang-format on
-    } // namespace regex_static
-
     constexpr char path_separator()
     {
 #if defined(_WIN32)
@@ -251,24 +208,6 @@ namespace internal {
 #else
         ::localtime_r(time, t);
 #endif
-    }
-
-    constexpr bool match_list_recursive_static(const char* s, const char* const patterns[], size_t count, size_t index)
-    {
-        return (count <= index)                                              ? false :
-            (clog::internal::regex_static::match_c(patterns[index], s) != 0) ? true :
-                                                                               match_list_recursive_static(s, patterns, count, index + 1);
-    }
-
-    constexpr bool match_list_static(const char* s, const char* const patterns[], size_t count)
-    {
-        return (s != nullptr) ? match_list_recursive_static(s, patterns, count, 0) : false;
-    }
-
-    template <typename Type, size_t Size>
-    constexpr size_t count_of(const Type (&)[Size])
-    {
-        return Size;
     }
 
     template <typename ValueType>
